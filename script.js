7bd47003119a3dde5ee863d83bda4a8f012e6af6
shivani-5548 <90037384+shivani-5548@users.noreply.github.com>
2023-10-21 18:42:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function returnPromise() {
+	let pro=new Promise((resolve)=>{
+		setTimeout(()=>{resolve("Hello, world!")},1000);
+	})
+	return pro;
+}
+
+let ele=document.getElementById("output");
+returnPromise().then((data)=>{ele.innerText=data;})
